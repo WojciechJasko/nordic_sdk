95378f8ff81e717eb28421021a000305d95f47dc
Wojciech Jasko <wojciech.jasko@nordicsemi.no>
2016-09-28 20:10:12 +0200

    add env_builder

---
+++ build_tools/env_builder.py
@@ -0,0 +1,77 @@
+import os
+import logging
+
+class EnvBuilder(object):
+    SCONS_DEFAULTS = [
+        'CC':          str(),
+        'CXX':         str(),
+        'AS':          str(),
+        'AR':          str(),
+        'LD':          str(),
+        'NM':          str(),
+        'OBJDUMP':     str(),
+        'OBJCOPY':     str(),
+        'SIZE':        str(),
+        'RANLIB':      str(),
+        'PROGSUFFIX':  str(),
+        'CCFLAGS':     list(),
+        'CFLAGS':      list(),
+    ]
+
+    def __init__(self, env):
+        self.env = env
+
+        # Complite the missing keys in enviroment
+        for key, value in desc.iteritems():
+            if key not in self.env:
+                env[key] = value
+
+
+    def create(self, desc):
+        # Create a copy
+        new = self.env.Clone()
+
+        self.default_update(env)
+
+        if 'fpu' in desc:
+            self.fpu_update(env, desc['fpu'])
+
+        if 'target' in desc:
+            self.target_update(env, desc['target'])
+
+        if 'build_type' in desc:
+            self.build_type_update(env, desc['build_type'])
+
+        return new
+
+
+    def build_type_update(self, env, choice):
+        if choice == "debug":
+            env["CCFLAGS"].extend([
+                                  "-O0",
+                                ])
+
+            env["CPPDEFINES"].extend([
+                           "NRF_DEBUG",
+                        ])
+
+        elif choice == "release":
+            env["CCFLAGS"].extend([
+                                  "-O3",
+                                ])
+
+        else:
+            raise Exception("Not supported build type: {}".format(choice))
+
+
+    def default_update(self, env):
+        pass
+
+
+    def fpu_update(self, env, choice):
+        pass
+
+
+    def target_update(self, env, choice):
+        pass
+
